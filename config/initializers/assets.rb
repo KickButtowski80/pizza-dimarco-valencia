0baf154b7e67fc29fdff82f5f89a80e601a1ffbc
kickbuttowski59 <pazpaz22@yahoo.com>
2018-06-03 20:34:35 +0000

    solved not showing image

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,3 +13,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w( about.css )
+Rails.application.config.assets.precompile += %w( menu.css )
+Rails.application.config.assets.precompile += %w( index.css )
