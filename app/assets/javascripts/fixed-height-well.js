e86c573e64065d55a15a0e09391ea31abd565ee0
kickbuttowski59 <pazpaz22@yahoo.com>
2018-06-05 02:47:06 +0000

    made contact us responsive and still needs some adjustments

---
+++ app/assets/javascripts/fixed-height-well.js
@@ -0,0 +1,16 @@
+
+
+$(document).on('turbolinks:load', function() {
+    
+    var max = 100%;
+
+    /* Getting the greatest height */
+    $(".well").each(function() {
+      max = ($(this).height() > max) ? $(this).height() : max;
+    });
+    
+    /* Applying the greatest height to each element */
+    $(".well").height(max);
+    
+    
+}
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,6 +12,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile += %w( about.css )
-Rails.application.config.assets.precompile += %w( menu.css )
-Rails.application.config.assets.precompile += %w( index.css )
+Rails.application.config.assets.precompile += %w( contact.scss )
+Rails.application.config.assets.precompile += %w( about.scss )
+Rails.application.config.assets.precompile += %w( menu.scss )
+Rails.application.config.assets.precompile += %w( index.scss )
