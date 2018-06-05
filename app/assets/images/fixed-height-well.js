796197527166472b95d8bdf15e0dec27fe76a43d
kickbuttowski59 <pazpaz22@yahoo.com>
2018-06-05 03:12:54 +0000

    moved js file to image folder

--- app/assets/images/fixed-height-well.js
+++ app/assets/images/fixed-height-well.js
@@ -2,15 +2,15 @@
 
 $(document).on('turbolinks:load', function() {
     
-    var max = 100%;
+    var max = 100%
 
     /* Getting the greatest height */
     $(".well").each(function() {
-      max = ($(this).height() > max) ? $(this).height() : max;
-    });
+      max = ($(this).height() > max) ? $(this).height() : max
+    })
     
     /* Applying the greatest height to each element */
-    $(".well").height(max);
+    $(".well").height(max)
     
     
 }
