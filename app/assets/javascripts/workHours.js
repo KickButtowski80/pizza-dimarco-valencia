d198a35ee5a6bdb756776f944b8668ecbc766af9
kickbuttowski59 <pazpaz22@yahoo.com>
2018-06-21 18:59:05 +0000

    choosing better naming in workhours js

--- app/assets/javascripts/workHours.js
+++ app/assets/javascripts/workHours.js
@@ -1,28 +1,32 @@
 $(document).on('turbolinks:load', function() {
         var d = new Date();
         var dayInNumber = d.getDay();
-        var myDiv = document.getElementById("workHours");
-        
+         var myClasses = document.getElementsByClassName("workHours");
+         var selectList;
+         var option;
+        // var myDiv = document.getElementsByTagName("span")
         //Create array of options to be added
         var array = 
         ["Sunday 11AM–10PM","Monday 11AM–10PM" ,"Tuesday 11AM–10PM","Wednesday 11AM–10PM","Thursday 11AM–10PM","Friday 11AM–11PM","Saturday 11AM–11PM"];
-        
-        //Create and append select list
-        var selectList = document.createElement("select");
-        selectList.id = "mySelect";
-        selectList.class = 'mytextwithicon';
-        myDiv.appendChild(selectList);
+        for (var i = 0; i < myClasses.length; i++) {
+                    //Create and append select list
+            selectList = document.createElement("select");
+            selectList.id = "mySelect";
+            selectList.class = 'mytextwithicon';
+            myDiv[i].appendChild(selectList);
+        }
+
         
         //Create and append the options
         for (var i = dayInNumber; i < array.length; i++) {
-            var option = document.createElement("option");
+            option = document.createElement("option");
             option.value = array[i];
             option.text = array[i];
             selectList.appendChild(option);
         }
         
         for (var i = 0 ; i < dayInNumber; i++) {
-            var option = document.createElement("option");
+            option = document.createElement("option");
             option.value = array[i];
             option.text = array[i];
             selectList.appendChild(option);
