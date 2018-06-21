548c70f658f4cb590411417ffefd4695e73a6bad
kickbuttowski59 <pazpaz22@yahoo.com>
2018-06-21 20:12:55 +0000

    working on hours drop down issue

--- app/assets/javascripts/workHours.js
+++ app/assets/javascripts/workHours.js
@@ -2,34 +2,35 @@ $(document).on('turbolinks:load', function() {
         var d = new Date();
         var dayInNumber = d.getDay();
          var myClasses = document.getElementsByClassName("workHours");
-         var selectList;
-         var option;
+         var selectList=[];
+         var option=[];
         // var myDiv = document.getElementsByTagName("span")
         //Create array of options to be added
         var array = 
         ["Sunday 11AM–10PM","Monday 11AM–10PM" ,"Tuesday 11AM–10PM","Wednesday 11AM–10PM","Thursday 11AM–10PM","Friday 11AM–11PM","Saturday 11AM–11PM"];
         for (var i = 0; i < myClasses.length; i++) {
                     //Create and append select list
-            selectList = document.createElement("select");
-            selectList.id = "mySelect";
-            selectList.class = 'mytextwithicon';
-            myClasses[i].appendChild(selectList);
+            selectList[i] = document.createElement("select");
+            selectList[i].setAttribute("class", "mySelect");
+            myClasses[i].appendChild(selectList[i]);
+            
+                        //Create and append the options
+            // for (var j = dayInNumber; j < array.length; j++) {
+            //     option = document.createElement("option");
+            //     option.value = array[j];
+            //     option.text = array[j];
+            //     selectList[i].appendChild(option);
+            // }
+            
+            // for (var j = 0 ; j < dayInNumber; j++) {
+            //     option = document.createElement("option");
+            //     option.value = array[j];
+            //     option.text = array[j];
+            //     selectList[i].appendChild(option);
+            // }
         }
 
         
-        //Create and append the options
-        for (var i = dayInNumber; i < array.length; i++) {
-            option = document.createElement("option");
-            option.value = array[i];
-            option.text = array[i];
-            selectList.appendChild(option);
-        }
-        
-        for (var i = 0 ; i < dayInNumber; i++) {
-            option = document.createElement("option");
-            option.value = array[i];
-            option.text = array[i];
-            selectList.appendChild(option);
-        }
+
 
 });
